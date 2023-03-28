74b58d7b0a488256f2611ee7702cda1f562511b1
Errno-404 <adamkmezydlo@gmail.com>
2023-03-28 22:29:45 +0200

    finally ended catcher, works properly now

---
+++ lab4/MezydloAdam/cw04/zad3/catcher.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/times.h>
+
+typedef enum {
+    ONE_HUNDRED = 1,
+    TIME_STAMP = 2,
+    REQUEST_COUNT = 3,
+    TIME_LOOP = 4,
+    SHUT_DOWN = 5,
+} MODE;
+
+MODE mode = ONE_HUNDRED;
+bool completed = true;
+int request_counter = 0;
+
+void single_time_stamp();
+void print_one_hundred();
+void print_request_counter();
+void time_stamp_loop();
+void shut_down();
+void handler(int signo, siginfo_t *info, void *context);
+
+
+int main() {
+    struct sigaction action;
+    sigemptyset(&action.sa_mask);
+    action.sa_sigaction = handler;
+    action.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &action, NULL);
+
+    printf("[CATCHER] Starting, PID= %d\n", getpid());
+
+    while (true) {
+        
+        if (completed)
+            {
+                // causes catcher to wait for signals!
+                pause();
+            }
+
+        switch (mode) {
+            case ONE_HUNDRED:
+                print_one_hundred();
+                break;
+            case TIME_STAMP:
+                single_time_stamp();
+                break;
+            case REQUEST_COUNT:
+                print_request_counter();
+                break;
+            case TIME_LOOP:
+                time_stamp_loop();
+                break;
+            case SHUT_DOWN:
+                shut_down();
+                break;
+            default:
+                printf("[CATCHER] Invalid mode!\n");
+                fflush(NULL);
+                completed = true;
+        }
+
+        if (mode != TIME_LOOP)
+            completed = true;
+    }
+
+    return 0;
+}
+
+void single_time_stamp() {
+    time_t raw_time = time(NULL);
+    struct tm *time_info = localtime(&raw_time);
+    printf("[CATCHER] Current timestamp: %s", asctime(time_info));
+    fflush(NULL);
+}
+
+void print_one_hundred() {
+    for (int i = 0; i < 100; i++)
+        printf("[CATCHER] %i\n", i + 1);
+    fflush(NULL);
+}
+
+void print_request_counter() {
+    printf("[CATCHER] Number of valid requests: %d\n", request_counter);
+    fflush(NULL);
+}
+
+void time_stamp_loop() {
+    clock_t start = clock();
+    double elapsed_time;
+    while (mode == TIME_LOOP) {
+        elapsed_time = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+        if (elapsed_time >= 1.0) {
+            single_time_stamp();
+            start = clock();
+        }
+    }
+}
+
+void shut_down() {
+    printf("[CATCHER] Ending.\n");
+    fflush(NULL);
+    exit(0);
+}
+
+void handler(int signo, siginfo_t *info, void *context) {
+    int sender = info->si_pid;
+    int request = info->si_value.sival_int;
+
+    if (request >= 1 && request <= 5) {
+        mode = (MODE)request;
+        request_counter++;
+        completed = false;
+    }
+    else {
+        printf("[CATCHER] Expected request in range: 1..5, got %d instead!\n", request);
+        fflush(NULL);
+        completed = true;
+    }
+
+    kill(sender, SIGUSR1);
+}
---
+++ lab4/MezydloAdam/cw04/zad3/sender.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIGNAL SIGUSR1
+#define INITIAL_STATE 2
+
+volatile int state_ack = INITIAL_STATE;
+
+void signal_handler(int signo)
+{
+    state_ack++;
+}
+
+bool register_signal_handler()
+{
+    struct sigaction sa = {0};
+    sa.sa_handler = signal_handler;
+
+    if (sigaction(SIGNAL, &sa, NULL) == -1) {
+        perror("[SENDER] Error registering signal handler");
+        return false;
+    }
+
+    return true;
+}
+
+bool send_signal(int catcher_pid, int state)
+{
+    sigval_t sig_val = {state};
+    if (sigqueue(catcher_pid, SIGNAL, sig_val) == -1) {
+        perror("[SENDER] Error sending signal");
+        return false;
+    }
+
+    printf("[SENDER] Sent state (%d).\n", state);
+    return true;
+}
+
+bool wait_for_ack(int state_index)
+{
+    time_t start_time = clock();
+    while (state_ack <= state_index)
+    {
+        if (clock() - start_time > CLOCKS_PER_SEC * 5)
+        {
+            printf("[SENDER] No confirmation, retrying.\n");
+            return false;
+        }
+    }
+
+    printf("[SENDER] Received confirmation.\n");
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "[SENDER] Invalid arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int catcher_pid = atoi(argv[1]);
+
+    for (int i = INITIAL_STATE; i < argc; i++)
+    {
+        int state = atoi(argv[i]);
+
+        if (!register_signal_handler())
+            exit(EXIT_FAILURE);
+
+        if (!send_signal(catcher_pid, state))
+            exit(EXIT_FAILURE);
+
+        if (!wait_for_ack(i))
+            i--;
+    }
+
+    return EXIT_SUCCESS;
+}
